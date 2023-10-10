392f36758c0658793f41836511a90ab573b99034
zTjamm <jamm85@bk.ru>
2023-10-10 21:00:52 +0300

    ok last fix'

--- lesson-01/05.js
+++ lesson-01/05.js
@@ -7,8 +7,8 @@ switch (true) {
     case rule.length < 25:
         console.log("Все таки нет правил без исключения");
         break;
-    case rule.length > 25:
-        console.log('У меня 100 пудов все получится');
+    case rule.length>25:
+        console.log("У меня 100 пудов все получится");
         break;
     case rule.length == 25:
         console.log("50 на 50");
