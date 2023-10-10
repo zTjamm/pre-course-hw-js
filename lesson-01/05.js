c21948b2e0bce52cffbc7b2ffc1527522448c121
zTjamm <jamm85@bk.ru>
2023-10-10 23:36:38 +0300

    not me

--- lesson-01/05.js
+++ lesson-01/05.js
@@ -1,15 +1,13 @@
 // code 05
 
 let rule = "Еще не родился тот человек, который поставил бы цель и не смог бы стать программистом.";
-console.log(rule.length)
 switch (true) {
     case rule.length < 25:
         console.log("Все таки нет правил без исключения");
         break;
-    case rule.length>25:
-        console.log('У меня 100 пудов все получится');
+    case rule.length > 25:
+        console.log("У меня 100 пудов все получится");
         break;
-    case rule.length === 25:
+    default:
         console.log("50 на 50");
-        break;
 }
