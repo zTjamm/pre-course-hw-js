0d3fcbcd450e6a985a0c2ac6c85114cb17cb712d
zTjamm <jamm85@bk.ru>
2023-10-10 20:08:27 +0300

    new js

--- lesson-01/03.js
+++ lesson-01/03.js
@@ -1,8 +1,2 @@
-// code
-let number1 = Math.floor(Math.random() * 100)
-
-if (number1 % 2 === 0) {
-    console.log(`${number1} : Это четное число`);
-} else {
-    console.log(`${number1}: Это нечетное число`)
-}
+// code 03
+console.log("my test")
