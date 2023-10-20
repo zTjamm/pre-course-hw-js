13289d8a97e9fb5f23f4481eaee91526f2d73bf0
zTjamm <jamm85@bk.ru>
2023-10-20 19:51:01 +0300

    les2-03 fix2

--- lesson-02/03.js
+++ lesson-02/03.js
@@ -1,9 +1,9 @@
 // code 03
 const giveMeRightNumbers = (value1, value2, maximum)=>{
     if (value1 === value2 || value1 <= maximum){
-        console.log('Success');
+        console.log('Success!');
     }else {
-        console.log("Something wrong");
+        console.log('Something wrong!');
     }
 }
 giveMeRightNumbers(300,110,200)
