d9263e4a6ee3e56d63ea44583250d9f9308f657f
zTjamm <jamm85@bk.ru>
2023-10-20 20:39:12 +0300

    les02-04 fix

--- lesson-02/05.js
+++ lesson-02/05.js
@@ -6,5 +6,5 @@ let passport = {
 let copyPassport = {...passport};
 copyPassport.name = "Ivan"
 
-console.log(passport);
-console.log(copyPassport);
+console.log(passport.name);
+console.log(copyPassport.name);
