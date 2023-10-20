26968b641ce9e59e30d9e9bb87558bec31ffab7e
zTjamm <jamm85@bk.ru>
2023-10-20 19:56:34 +0300

    les02-04

---
+++ lesson-02/04.js
@@ -0,0 +1,10 @@
+// code 04
+const giveMeRightCondition = (value1, value2, maximum) => {
+    if (value1 === value2 && value1 <= maximum) {
+        console.log('Success!');
+    } else {
+        console.log('Something wrong!');
+    }
+}
+
+giveMeRightCondition(100, 110, 200)
