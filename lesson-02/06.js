ac2ca1ccb2859a279a57bad86f7d791adc15b2eb
zTjamm <jamm85@bk.ru>
2023-10-21 14:58:10 +0300

    les02-06

---
+++ lesson-02/06.js
@@ -0,0 +1,13 @@
+let passportWithAddress = {
+    name: "Petr",
+    surname: "Petrov",
+    address: {
+        country: "USA",
+        city: "LA"
+    }
+};
+let clone = structuredClone(passportWithAddress)
+clone.address.city = 'Bobryisk';
+
+console.log(passportWithAddress)
+console.log(clone)
