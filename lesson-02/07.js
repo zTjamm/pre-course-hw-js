0e8b4a41fb3705b3118c9f3e869f88a2102a960d
zTjamm <jamm85@bk.ru>
2023-10-21 15:50:39 +0300

    les02-07

--- lesson-02/07.js
+++ lesson-02/07.js
@@ -8,7 +8,7 @@ let passportMarried = {
     }
 };
 let passportMarried2 = { ...passportMarried, married: true, address: { ...passportMarried } }
-passportMarried2.married = "true"
+passportMarried2.married = true;
 
 console.log(passportMarried);
 console.log(passportMarried2);
