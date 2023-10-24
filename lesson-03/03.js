75aa356a91fcacca31a503238825e699f5977862
zTjamm <jamm85@bk.ru>
2023-10-24 16:44:50 +0300

    les03-03

---
+++ lesson-03/03.js
@@ -0,0 +1,4 @@
+const fruits =[ "яблоко", "банан", "апельсин", "манго", "киви", "ананас", "виноград", "груша", "слива", "персик"];
+
+const sliceFruits = fruits.slice(1,8);
+console.log(sliceFruits)
