644f317fd366db237a6789cbe1c3b4059d0fac06
zTjamm <jamm85@bk.ru>
2023-10-24 16:26:30 +0300

    les03-01

--- lesson-03/01.js
+++ lesson-03/01.js
@@ -1,1 +1,6 @@
-// code
+const colors = ["синий", "черный", "зеленый", "красный", "желтый", "зеленый"];
+
+function createColorString() {
+    return colors.filter(c=> c==="черный" || c === "красный" || c === "желтый");
+}
+console.log(createColorString());
