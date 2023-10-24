c3ae3e6b30b4521529af4b02a280f4f138d3a596
zTjamm <jamm85@bk.ru>
2023-10-24 16:29:09 +0300

    les03-01 fix

--- lesson-03/01.js
+++ lesson-03/01.js
@@ -1,6 +1,6 @@
 const colors = ["синий", "черный", "зеленый", "красный", "желтый", "зеленый"];
 
 function createColorString() {
-    return colors.filter(c=> c==="черный" || c === "красный" || c === "желтый");
+    return colors.filter(c=> c==="черный" || c === "красный" || c === "желтый").join('-');
 }
 console.log(createColorString());
